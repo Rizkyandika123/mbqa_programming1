217315e7ba8edf6f0a55ef367d740e67e2bb32a7
Rizky Andika <iputurizkyandika@gmail.com>
2023-12-12 06:00:29 +0800

    first commit

---
+++ src/test/test.java
@@ -0,0 +1,27 @@
+package test;
+
+public class test {
+
+  public static void main(String[] args) {
+    for (int i=0; i<5; i++) {
+    	for(int j=0; j<5; j++) {
+    		System.out.print("* ");
+    		if(i==j) {
+    			break;
+    		}
+    	}
+    	System.out.print("\n");
+    }
+    
+    for (int i=0; i<5; i++) {
+    	for(int j=0; j<5; j++) {
+    		System.out.print("* ");
+    		if((i+j)==4) {
+    			break;
+    		}
+    	}
+    	System.out.print("\n");
+    }
+    
+  }
+}
